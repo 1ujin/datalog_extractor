31b92796f640c7dea5ebb4764b083776aff43934
1ujin <lujin.vip@foxmail.com>
2026-05-14 16:32:03 +0800

    build: 先判断app/resource.py是否存在，只有不存在时才生成

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 rem @echo off
 rem 编译资源文件
-start pyrcc5 -o ./src/resource.py ./resource/resource.qrc
+if not exist app/resource.py (
+    start pyrcc5 -o ./src/resource.py ./resource/resource.qrc
+)
 rem 生成可执行文件
 start pyinstaller ./datalog_extractor.spec
